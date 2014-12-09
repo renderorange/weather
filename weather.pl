865d5eeff8a56009d5e5dc49fa012f52622216f1
Blaine Motsinger <blaine@renderorange.com>
2014-12-09 08:47:07 -0600

    updated to receive zip, cleaned up a bit

--- weather.pl
+++ weather.pl
@@ -5,40 +5,52 @@
 use strict;
 use warnings;
 use Getopt::Long;
-use local::lib;
 use LWP::Simple;
 use JSON::XS;
 
-use Data::Dumper;
-
 # process commandline options
-my ($forecast, $help);
-GetOptions ( "forecast" => \$forecast,
-             "help" => \$help );
-if ($help) { help(); }
+my ($zip, $current, $forecast, $help);
+GetOptions ( "z|zip=i" => \$zip,
+             "c|current" => \$current,
+             "f|forecast" => \$forecast,
+             "h|help" => \$help );
+if (! $zip || $help) { help(); }
+if ($current || $forecast) { 
+    eval 1;  # find more elegant way to do this
+} else {
+    help();
+}
 
-# wunderground api query settings
+# wunderground api settings
 my $api_key = '';
-my $state = 'TX';
-my $city = 'Cypress';
+if (! $api_key) {
+    print "api key not set\n";
+    exit;
+}
 
-# query wunderground, decode, then store and retrieve data
+# get location data for zip
+my $locations_hash = decode_json get("http://api.wunderground.com/api/$api_key/geolookup/q/$zip.json");
+my $city = $locations_hash->{'location'}->{'city'};
+my $state = $locations_hash->{'location'}->{'state'};
+
+# query wunderground, decode, then store data
 my ($conditions_hash, $forecast_hash);
-$conditions_hash = decode_json execute_api_query('conditions');
+if ($current) { $conditions_hash = decode_json execute_api_query('conditions'); }
 if ($forecast) { $forecast_hash = decode_json execute_api_query('forecast'); }
 
 # output
-print "weather for $city, $state\n" .
-      "\n" .
-      "currently:  $conditions_hash->{'current_observation'}->{'weather'}\n" .
-      "temp:  $conditions_hash->{'current_observation'}->{'temperature_string'}\n" .
-      "humidity:  $conditions_hash->{'current_observation'}->{'relative_humidity'}\n" .
-      "precip:  $conditions_hash->{'current_observation'}->{'precip_today_string'}\n" .
-      "wind:  $conditions_hash->{'current_observation'}->{'wind_string'}\n" .
-      "visibility:  $conditions_hash->{'current_observation'}->{'visibility_mi'} miles\n" .
-      "\n";
+print "weather for $city, $state\n\n";
+if ($current) {
+    print "currently:  $conditions_hash->{'current_observation'}->{'weather'}\n" .
+          "temp:  $conditions_hash->{'current_observation'}->{'temperature_string'}\n" .
+          "humidity:  $conditions_hash->{'current_observation'}->{'relative_humidity'}\n" .
+          "precip:  $conditions_hash->{'current_observation'}->{'precip_today_string'}\n" .
+          "wind:  $conditions_hash->{'current_observation'}->{'wind_string'}\n" .
+          "visibility:  $conditions_hash->{'current_observation'}->{'visibility_mi'} miles\n" .
+          "\n";
+}
 if ($forecast) { 
-    print "forecast for:\n" .
+    print "4 day forecast\n" .
           "$forecast_hash->{'forecast'}->{'txt_forecast'}->{'forecastday'}->[0]{'title'}\n" .
           "    AM: $forecast_hash->{'forecast'}->{'txt_forecast'}->{'forecastday'}->[0]{'fcttext'}\n" .
           "    PM: $forecast_hash->{'forecast'}->{'txt_forecast'}->{'forecastday'}->[1]{'fcttext'}\n" .
@@ -54,18 +66,16 @@ if ($forecast) {
           "\n";
 }
 
-# testing
-#print "\n";
-#print "[testing]\n";
-#print "dumper output for \$forecast\n";
-#print Dumper $forecast;
-#print "\n";
-
 # subs
 sub help {
-    die "usage: ./weather.pl\n" .
-          "-f|--forecast\tdisplay 4 day forecast\n" .
-          "-h|--help\tdisplays this dialogue\n" .
+    die "usage: ./weather.pl -z 77429 -c -f\n\n" .
+          "required\n" .
+          "  -z|--zip\tpostal code of the location to check\n" .
+          "\n" .
+          "optional (either -c or -f must be passed)\n" .
+          "  -c|--current\tdisplays current conditions\n" .
+          "  -f|--forecast\tdisplays 4 day forecast\n" .
+          "  -h|--help\tdisplays this dialogue\n" .
           "\n";
 }
 
@@ -74,7 +84,7 @@ sub execute_api_query {
     my $uri = "http://api.wunderground.com/api/$api_key/$function/q/$state/$city.json";
     my $results = get($uri);
     if (! $results) {
-        die 'api query returned null';
+        die "api query returned null\n";
     } else { 
         return $results;
     }
