83fcfa4ddd4028d9fc0cc3804185bc7e00321ae9
Blaine Motsinger <blaine@renderorange.com>
2019-01-30 23:50:37 -0600

    stripped to no functionality to start from scratch
    only starting with pod and options to plan out the re-write.

--- weather.pl
+++ weather.pl
@@ -1,121 +1,93 @@
 #!/usr/bin/env perl
 
-# weather.pl
-# github.com/renderorange/weather
-
 use strict;
 use warnings;
 
-use local::lib;
-use Getopt::Long;
-use File::Slurper 'read_lines';
-use LWP::Simple;
-use Cpanel::JSON::XS;
-use Term::ANSIColor qw(:constants);
-$Term::ANSIColor::AUTORESET = 1;
+use Getopt::Long ();
+use Pod::Usage   ();
 
-# process commandline options
-my ($zip, $forecast, $help);
-GetOptions ( "z|zip=i" => \$zip,
-             "f|forecast" => \$forecast,
-             "h|help" => \$help );
-if ($help) { help(); }
+my $VERSION = '2.00';
 
-# make sure the rc file is there
-if (! -e '.weather.rc') {
-    print ".weather.rc is not present\n" .
-          "please see github.com/renderorange/weather for setup details\n\n";
-    exit 1;
+my $opt = {};
+
+Getopt::Long::GetOptions(
+    "zip=i"    => \$opt->{zip},
+    "current"  => \$opt->{current},
+    "forecast" => \$opt->{forecast},
+    "version"  => \$opt->{version},
+    "help"     => \$opt->{help},
+    "man"      => \$opt->{man},
+) or Pod::Usage::pod2usage( -exitval => 1, -verbose => 0 );
+
+Pod::Usage::pod2usage( -exitval => 0, -verbose => 1 ) if $opt->{help};
+Pod::Usage::pod2usage( -exitval => 0, -verbose => 2, noperldoc => 1 ) if $opt->{man};
+
+if ( $opt->{version} ) {
+    print "$VERSION\n";
+    exit 0;
 }
 
-# load and verify config
-my %config;
-foreach (read_lines('.weather.rc')) {
-    if (/^#/) { next; }  # filter out comments  # [TODO] filter out blank lines
-    my ($key, $value) = split (/:/);  # store key and value pairs
-    # trim whitespace
-    $key =~ s/^\s+|\s+$//g;    # [TODO] this here isn't very elegant
-    $value =~ s/^\s+|\s+$//g;  # I tried to use map on the split list above, but it didn't like modifying $_
-    # verify config contains what's expected
-    if ($key !~ /^api_key$/ || $value !~ /^\w+$/) {
-        print ".weather.rc doesn't appear to contain what's needed\n" .
-              "please see github.com/renderorange/weather for setup details\n\n";
-        exit 1;
-    }
-    $config{$key} = $value;  # $config{'api_key'} = wunderground key is accessed like so
-}
+Pod::Usage::pod2usage( -exitval => 1, -verbose => 0 ) if ( !$opt->{current} || !$opt->{forecast} );
 
-# get current zip
-my $geolocation_hash;
-if (! $zip) {  # if zip isn't set by the user, get the information from geolocation
-    $geolocation_hash = decode_json execute_api_query('http://ip-api.com/json');
-    $zip = $geolocation_hash->{'zip'};
-}
+__END__
 
-# get and set city and state
-my $location_hash = decode_json execute_api_query('http://api.wunderground.com/api', $config{'api_key'}, 'geolookup', "q/$zip.json");
-my $city = $location_hash->{'location'}->{'city'};
-my $state = $location_hash->{'location'}->{'state'};
+=pod
 
-# query wunderground, decode, then store data
-my ($conditions_hash, $forecast_hash);
-$conditions_hash = decode_json execute_api_query('http://api.wunderground.com/api', $config{'api_key'}, 'conditions', "q/$state/$city.json");
-if ($forecast) { $forecast_hash = decode_json execute_api_query('http://api.wunderground.com/api/', $config{'api_key'}, 'forecast', "q/$state/$city.json") }
+=head1 NAME
 
-# output
-print BOLD "\nweather for $city, $state\n\n";
-print "currently:  $conditions_hash->{'current_observation'}->{'weather'}\n" .
-      "temp:  $conditions_hash->{'current_observation'}->{'temperature_string'}\n" .
-      "humidity:  $conditions_hash->{'current_observation'}->{'relative_humidity'}\n" .
-      "precip:  $conditions_hash->{'current_observation'}->{'precip_today_string'}\n" .
-      "wind:  $conditions_hash->{'current_observation'}->{'wind_string'}\n" .
-      "visibility:  $conditions_hash->{'current_observation'}->{'visibility_mi'} miles\n" .
-      "\n";
-if ($forecast) { 
-    print BOLD "4 day forecast\n\n";
-    print "$forecast_hash->{'forecast'}->{'txt_forecast'}->{'forecastday'}->[0]{'title'}\n" .
-          "AM:  $forecast_hash->{'forecast'}->{'txt_forecast'}->{'forecastday'}->[0]{'fcttext'}\n" .
-          "PM:  $forecast_hash->{'forecast'}->{'txt_forecast'}->{'forecastday'}->[1]{'fcttext'}\n\n" .
-          "$forecast_hash->{'forecast'}->{'txt_forecast'}->{'forecastday'}->[2]{'title'}\n" .
-          "AM:  $forecast_hash->{'forecast'}->{'txt_forecast'}->{'forecastday'}->[2]{'fcttext'}\n" .
-          "PM:  $forecast_hash->{'forecast'}->{'txt_forecast'}->{'forecastday'}->[3]{'fcttext'}\n\n" .
-          "$forecast_hash->{'forecast'}->{'txt_forecast'}->{'forecastday'}->[4]{'title'}\n" .
-          "AM:  $forecast_hash->{'forecast'}->{'txt_forecast'}->{'forecastday'}->[4]{'fcttext'}\n" .
-          "PM:  $forecast_hash->{'forecast'}->{'txt_forecast'}->{'forecastday'}->[5]{'fcttext'}\n\n" .
-          "$forecast_hash->{'forecast'}->{'txt_forecast'}->{'forecastday'}->[6]{'title'}\n" .
-          "AM:  $forecast_hash->{'forecast'}->{'txt_forecast'}->{'forecastday'}->[6]{'fcttext'}\n" .
-          "PM:  $forecast_hash->{'forecast'}->{'txt_forecast'}->{'forecastday'}->[7]{'fcttext'}\n" .
-          "\n";
-}
+weather - cli program to get the weather for your location
 
-# subs
-sub help {
-    print "usage: ./weather.pl -z 77429 -f\n\n" .
-          "options:\n" .
-          "\t--zip\t\tspecify where you want weather results for\n" .
-          "\t\t\twithout --zip, weather.pl performs a geolookup to find where you are\n\n" .
-          "\t--forecast\tadditionally displays the next 4 day forecast\n\n" .
-          "\t--help\t\tdisplays this dialogue\n\n";
-    exit 1;
-}
+=head1 SYNOPSIS
 
-sub execute_api_query {
-    my ($url, $key, $function, $query) = @_;
-    my $uri;
-    if (scalar(@_) == 1) {  # for now, the only API we're consuming other than wunderground doesn't require auth
-        $uri = $url;        # so, keep it simple and just run the url
-    } else {                # otherwise assume it's going to be wunderground 
-        $uri = "$url" . '/' . "$key" . '/' . "$function" . '/' . "$query";
-    }
-    my $res = get($uri);
-    if (! $res) {
-        print "the api query to $url returned null\n";
-        exit 1;
-    } elsif ($res =~ /keynotfound/) {
-        print "the wunderground key in your .weather.rc doesn't exist at wunderground.\n" .
-              "please see github.com/renderorange/weather for setup details\n\n";
-        exit 1;
-    } else {
-        return $res;
-    }
-}
+  weather [--zip] <int>
+          [--current] [--forecast]
+          [--version]
+          [--help] [--man]
+
+=head1 DESCRIPTION
+
+This program will retrieve weather condition and forecast details for your location.
+
+=head1 OPTIONS
+
+  --zip         specify where you want weather results for
+                geolocation lookup will be performed without zip
+  --current     print the current conditions
+  --forecast    print the next 4 day forecast
+  --version     print the version and exit
+  --help        print this dialogue
+  --man         display the full documentation
+
+=head1 EXAMPLES
+
+=over
+
+=item get the current conditions for a specified zip code
+
+  weather --zip 77095 --current
+
+=item get the current conditions and 4 day forecast
+
+  weather --zip 77095 --current --forecast
+
+=item get the 4 day forecast only, via geolocation
+
+  weather --forecast
+
+=back
+
+=head1 DEPENDENCIES
+
+=over
+
+=item Getopt::Long
+
+=item Pod::Usage
+
+=back
+
+=head1 AUTHOR
+
+Blaine Motsinger, <blaine@renderorange.com>
+
+=cut
